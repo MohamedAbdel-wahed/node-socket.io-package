76e07f99e8ef82e96ca1b5b494989de4deb1053e
MohamedAbdel-wahed <webdeveloper.int99@gmail.com>
2021-06-13 16:20:09 +0000

    new-update

--- app.js
+++ app.js
@@ -1,63 +1,39 @@
 const moment = require("moment")
 const cors = require("cors")
 const express = require("express")
-const { connect } = require("mongoose")
 const app = express()
 const server = require("http").Server(app)
+const axios =require("axios")
 const io = require("socket.io")(server, {
 	cors: "*"
 })
-const fetchApi = require("node-fetch")
-const axios =require("axios")
 
-const messageRoutes = require("./routes/message")
 const { addUser, getUser } = require("./users")
-const Message = require("./models/message")
-const Location = require("./models/location")
 
 
 app.use(cors())
-app.use("/uploads", express.static("uploads"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const mongoUrl = `mongodb+srv://mohamed:test1234@cluster0.vh16z.mongodb.net/ws?retryWrites=true&w=majority`
-
- connect(mongoUrl, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useCreateIndex: true,
-})
-	.then(() => {
-		console.log("Mongodb Connected Successfully")
-		server.listen(process.env.PORT || 5000, () =>
-			console.log(`Server is ready on port 5000`)
-		)
-	})
-	.catch((err) => console.log(err))
-
-app.post("/api/chat", messageRoutes)
-app.use("/api/chat", messageRoutes)
+ 
+server.listen(process.env.PORT || 5000, () =>
+	console.log(`Server is ready on port 5000`)
+)
 
 const main = async () => {
-	const response= await axios.get("https://pina-app.com/api/chat/users")
+	const endpoint= "https://pina-app.com"
+	// const endpoint= "http://127.0.0.1:8000" 
+
+	const response= await axios.get(`${endpoint}/api/chat/users`)
 	const users = await response.data.data
 
-	// console.log(users)
-
-	axios.post('https://pina-app.com/api/chat/add-message', {username: "John Doe"})
-		.then(res => {
-			console.log(res?.data || res)
-			console.log("new message sent")
-		})
-		.catch(err => console.log(err))
+	// console.log(users) 
 
 	io.on("connection", (socket) => {
 			console.log(`new user connected!`)
 		socket.on("join", ({ userId, room }) => {
 			console.log("start db")
 			const db_user = users.find(user => user.id===userId && user.village_id === parseInt(room))
-			console.log(db_user)
 			if (!db_user) {
 				io.emit("unjoin", { status: 401 })
 				return;
@@ -78,45 +54,29 @@ const main = async () => {
 
 			socket.join(user.room)
 		})
-	
-		socket.on("chat:send", async (data) => {
+
+		socket.on("chat:send", async ({userId,username,type,text,url,lat,long}) => {
 			console.log("sending new message...")
 			const user = getUser(socket.id)
 			if (!user) return { message: "not authroized to enter this room" }
 
-			axios.post('https://pina-app.com/api/chat/add-message', data)
+			axios.post(`${endpoint}/api/chat/add-message`, {user_id: userId,username,type,text,url,lat,long,village_id: user.room})
 				.then(res => {
-					console.log(res?.data || res)
-					console.log("new message sent")
+					const data= res?.data?.data || null
+					console.log(data)
+					data.created_at = moment(data?.created_at).fromNow() || ""
+					io.to(data.village_id).emit("chat:message", data)
 				})
 				.catch(err => console.log(err))
-			
-			console.log("after request")
-		
-			// const { _doc } = await Message.create(data)
-	
-			// io.to(user.room).emit("chat:message", {
-			// 	..._doc,
-			// 	// createdAt: moment(_doc.createdAt).fromNow(),
-			// })
 		})
 
-		socket.on("currentLocation", async ({ userId, lat, long }) => {
-			let location = null;
-			location = await Location.find({ userId })
-			if (location) {
-				location.lat = lat;
-				location.long = long;
-			}
-			else {
-				location= await Location.create({userId, lat, long})
-			}
-
-			location.save();
-
-			io.emit("upadatedLocation", {
-				...location._doc,
-				createdAt: moment(_doc.createdAt).fromNow(),
+		socket.on("currentLocation", ({user_id, lat, long}) => {
+			console.log({user_id, lat, long})
+			axios.post(`${endpoint}/api/current-location`, {user_id, lat, long})
+			.then(res => {
+				console.log("new location has been set!");
+				console.log(res?.data || res)
+				io.emit("upadatedLocation", res?.data?.data)
 			})
 		})
 		
--- uploads/readme.txt
+++
@@ -1,3 +0,0 @@
-this file is created 
-because we don't want (uploads DIR) to be empty 
-as heroku deletes empty folders
