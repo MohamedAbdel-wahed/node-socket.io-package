91a0dcd377a5373f0354891d459b7d83a79871e9
MohamedAbdel-wahed <webdeveloper.int99@gmail.com>
2021-07-12 15:49:33 +0000

    new

--- app.js
+++ app.js
@@ -23,85 +23,84 @@ app.get("/home", (req, res) => {
 });	
 
 const main = async () => {
-  // const endpoint = "https://pina-app.com";
-  const endpoint= "http://127.0.0.1:8000"
+  const endpoint = "https://pina-app.com";
+  // const endpoint = "http://127.0.0.1:8000"
 
-  const response = await axios.get(`${endpoint}/api/chat/users`);
+  const response = await axios(`http://localhost:8000/api/chat/users`)
   const users = await response?.data?.data;
 
-  console.log(users)
+  users.filter(user => user.village_id)
+  
+    io.on("connection", (socket) => {
+      console.log(`new user connected!`);
+      socket.on("join", ({ userId, room }) => {
+        console.log("start db");
+        const db_user = users.find(
+          (user) => user.id === userId && user.village_id === parseInt(room)
+        );
+        if (!db_user) {
+          io.emit("unjoin", { status: 401 });
+          return;
+        }
 
+        console.log(`new user just joined!`);
 
-  io.on("connection", (socket) => {
-    console.log(`new user connected!`);
-    socket.on("join", ({ userId, room }) => {
-      console.log("start db");
-      const db_user = users.find(
-        (user) => user.id === userId && user.village_id === parseInt(room)
-      );
-      if (!db_user) {
-        io.emit("unjoin", { status: 401 });
-        return;
-      }
-
-      console.log(`new user just joined!`);
-
-      const { user, error } = addUser({
-        id: socket.id,
-        username: db_user.username,
-        room: db_user.village_id,
-      });
-      socket.emit("chat:message", {
-        username: "admin",
-        text: `Hi ${user.username}, Welcome to the chat!`,
-      });
-
-      socket.broadcast.to(user.room).emit("message", {
-        username: "admin",
-        text: `${user.username} has joined the chat!`,
-      });
-
-      socket.join(user.room);
-    });
-
-    socket.on(
-      "chat:send",
-      async ({ userId, username, type, text, url, lat, long }) => {
-        console.log("sending new message...");
-        const user = getUser(socket.id);
-        if (!user) return { message: "not authroized to enter this room" };
-
-        axios
-          .post(`${endpoint}/api/chat/add-message`, {
-            user_id: userId,
-            username,
-            type,
-            text,
-            url,
-            lat,
-            long,
-            village_id: user.room,
-          })
-          .then((res) => {
-            const data = res?.data?.data || null;
-            console.log(data);
-            io.to(data.village_id).emit("chat:message", data);
-          })
-          .catch((err) => console.log(err));
-      }
-    );
-
-    socket.on("currentLocation", ({ user_id, lat, long }) => {
-      console.log({ user_id, lat, long });
-      axios
-        .post(`${endpoint}/api/current-location`, { user_id, lat, long })
-        .then((res) => {
-          console.log("new location has been set!");
-          console.log(res?.data || res);
-          io.emit("upadatedLocation", res?.data?.data);
+        const { user, error } = addUser({
+          id: socket.id,
+          username: db_user.username,
+          room: db_user.village_id,
         });
+        socket.emit("chat:message", {
+          username: "admin",
+          text: `Hi ${user.username}, Welcome to the chat!`,
+        });
+
+        socket.broadcast.to(user.room).emit("message", {
+          username: "admin",
+          text: `${user.username} has joined the chat!`,
+        });
+
+        socket.join(user.room);
+      });
+
+      socket.on(
+        "chat:send",
+        async ({ userId, username, type, text, url, lat, long }) => {
+          console.log("sending new message...");
+          const user = getUser(socket.id);
+          if (!user) return { message: "not authroized to enter this room" };
+
+          axios
+            .post(`${endpoint}/api/chat/add-message`, {
+              user_id: userId,
+              username,
+              type,
+              text,
+              url,
+              lat,
+              long,
+              village_id: user.room,
+            })
+            .then((res) => {
+              const data = res?.data?.data || null;
+              console.log(data);
+              io.to(data.village_id).emit("chat:message", data);
+            })
+            .catch((err) => console.log(err));
+        }
+      );
+
+      socket.on("currentLocation", ({ user_id, lat, long }) => {
+        console.log({ user_id, lat, long });
+        axios
+          .post(`${endpoint}/api/current-location`, { user_id, lat, long })
+          .then((res) => {
+            console.log("new location has been set!");
+            console.log(res?.data || res);
+            io.emit("upadatedLocation", res?.data?.data);
+          });
+      });
     });
-  });
 };
 
 main();
